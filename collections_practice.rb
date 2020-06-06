50fce31d64fa39f6b444b9b1228da20ff426fa02
Flora Petersen <floramarina@comcast.net>
2020-06-06 05:34:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,15 @@ end
 def sort_array_desc(array)
   array.sort.reverse
 end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+  
